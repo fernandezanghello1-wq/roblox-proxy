cc04e29c752e83a6c7ebc8412ad29d67fb1b7c8b
SAMUEL <fernandezanghello1@gmail.com>
2026-05-14 21:03:00 -0500

    updated server

--- server.js
+++ server.js
@@ -5,10 +5,48 @@ const cors = require("cors");
 const app = express();
 
 app.use(cors());
+app.use(express.json());
 
 const SECRET_KEY = "DJCHEROO_SECRET";
 
+app.get("/", (req, res) => {
+    res.send("Proxy funcionando 😎");
+});
+
 app.get("/proxy", async (req, res) => {
+
+    try {
+
+        const key = req.query.key;
+        const url = req.query.url;
+
+        if (key !== SECRET_KEY) {
+            return res.status(403).send("Unauthorized");
+        }
+
+        if (!url) {
+            return res.status(400).send("Missing URL");
+        }
+
+        const response = await axios.get(url, {
+            headers: {
+                "User-Agent": "Mozilla/5.0"
+            }
+        });
+
+        res.json(response.data);
+
+    } catch (err) {
+
+        console.log(err);
+
+        res.status(500).send("Proxy Error");
+
+    }
+
+});
+
+app.post("/webhook", async (req, res) => {
 
     try {
 
@@ -18,28 +56,25 @@ app.get("/proxy", async (req, res) => {
             return res.status(403).send("Unauthorized");
         }
 
-        const url = req.query.url;
+        const webhook =
+            "https://discord.com/api/webhooks/1504662497704284232/NKPYplxnYxtvBSDkLQIoqn9Yj1Xrrn97bhAtmUcU_Uia4HoYHBEun7cFqliSnrrLeNYf";
 
-        if (!url) {
-            return res.status(400).send("No URL");
-        }
+        await axios.post(webhook, req.body);
 
-        const response = await axios.get(url);
-
-        res.json(response.data);
+        res.send("Webhook enviado 😎");
 
     } catch (err) {
 
         console.log(err);
 
-        res.status(500).send("Error");
+        res.status(500).send("Webhook Error");
 
     }
 
 });
 
-app.listen(3000, () => {
+const PORT = process.env.PORT || 3000;
 
-    console.log("Secure proxy funcionando");
-
+app.listen(PORT, () => {
+    console.log("Proxy funcionando en puerto " + PORT);
 });
